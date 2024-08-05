f0ff0262847ea9aa51f173edfc4d39544cc50b62
hbach089 <hbach089@uottawa.ca>
2024-08-05 01:18:37 -0400

    onclick filter by Type

--- movieapp.js
+++ movieapp.js
@@ -1,9 +1,13 @@
-let a=0;
 
 let div=document.createElement('div');
 div.setAttribute("id", "divwithclasses");
 
+let lol='wtf',clicked=false;
+
 async function DisplayMovies(){
+  if(div.innerHTML){
+    div.innerHTML='';
+  }
   let word=document.getElementById('searchit').value;/*localStorage.getItem('word')*/
   sessionStorage.removeItem("reloading");
     const url = 'https://movie-database-alternative.p.rapidapi.com/?s='+word+'&r=json&page=2';
@@ -20,44 +24,96 @@ async function DisplayMovies(){
       const result = await response.json();
       // console.log(result);
       // console.log(result.Search);
-
-     
-      
+      lol='nice';
+            
       for(let i in result.Search){
+        
+        if(option=='home'){
+          let classdiv = document.createElement('div');
+          classdiv.classList.add('divit');
 
-        let classdiv = document.createElement('div');
-        classdiv.classList.add('divit');
+          let figElem = document.createElement('figure');
+          let imageElem = document.createElement('img');
+          imageElem.src = result.Search[i].Poster;
 
-        let figElem = document.createElement('figure');
-        let imageElem = document.createElement('img');
-        imageElem.src = result.Search[i].Poster;
+          let checkImageSrc=imageElem.src.slice(imageElem.src.length-3,imageElem.src.length);
+          if(checkImageSrc=='N/A'){
+            continue;
+          }
+          let figCaptionElem = document.createElement('figcaption');
+          // figCaptionElem.textContent = result.Search[i].Title+", "+result.Search[i].Year+"\r\n"+result.Search[i].Type;
+          figCaptionElem.innerHTML=result.Search[i].Title+", "+result.Search[i].Year+"<br>"+result.Search[i].Type;
 
-        let checkImageSrc=imageElem.src.slice(imageElem.src.length-3,imageElem.src.length);
-        if(checkImageSrc=='N/A'){
-          continue;
+          figElem.appendChild(imageElem);
+          figElem.appendChild(figCaptionElem);
+          // console.log(figElem);
+          
+          classdiv.appendChild(figElem);
+          document.body.appendChild(classdiv);
+          // console.log(figElem);
+          div.appendChild(classdiv);
+          console.log(result.Search[i].Type);
         }
-        let figCaptionElem = document.createElement('figcaption');
-        figCaptionElem.textContent = result.Search[i].Title+", "+result.Search[i].Year;
+        else if(option==result.Search[i].Type){
+          let classdiv = document.createElement('div');
+          classdiv.classList.add('divit');
+
+          let figElem = document.createElement('figure');
+          let imageElem = document.createElement('img');
+          imageElem.src = result.Search[i].Poster;
+
+          let checkImageSrc=imageElem.src.slice(imageElem.src.length-3,imageElem.src.length);
+          if(checkImageSrc=='N/A'){
+            continue;
+          }
+          let figCaptionElem = document.createElement('figcaption');
+          figCaptionElem.textContent = result.Search[i].Title+", "+result.Search[i].Year;
+          
+          figElem.appendChild(imageElem);
+          figElem.appendChild(figCaptionElem);
+          // console.log(figElem);
+          
+          classdiv.appendChild(figElem);
+          document.body.appendChild(classdiv);
+          // console.log(figElem);
+          div.appendChild(classdiv);
+          console.log(result.Search[i].Type);
+        }
         
-        figElem.appendChild(imageElem);
-        figElem.appendChild(figCaptionElem);
-        // console.log(figElem);
-        
-        classdiv.appendChild(figElem);
-        document.body.appendChild(classdiv);
-        // console.log(figElem);
-        div.appendChild(classdiv);
-        document.body.appendChild(div);
       }
+      document.body.appendChild(div);
+
       
     } catch (error) {
       console.error(error);
     }
-  div.scrollIntoView();
-  
-  let d=document.createElement('h2');
-  d.innerHTML="FEATURED";
-  document.body.appendChild(d);
+  // div.scrollIntoView();
+  console.log(option+'  and   '+clicked+" LOOOOOLmao");
+  if(option=='home' && clicked==true){
+    div.scrollIntoView();  
+    // let d=document.createElement('h2');
+    d.innerHTML="FEATURED";
+    document.body.appendChild(d);
+  }
+  else if(option=='movie' && clicked==true){
+    div.scrollIntoView();  
+    // let d=document.createElement('h2');
+    d.innerHTML="movies";
+    document.body.appendChild(d);
+  }
+  else if(option=='series' && clicked==true){
+    div.scrollIntoView();  
+    // let d=document.createElement('h2');
+    d.innerHTML="TV";
+    document.body.appendChild(d);
+  }
+  else if(option=='game' && clicked==true){
+    div.scrollIntoView();  
+    // let d=document.createElement('h2');
+    d.innerHTML="GAMING";
+    document.body.appendChild(d);
+  }
+  clicked=false;
 }
 
 
@@ -66,11 +122,11 @@ document.getElementById('btnit').addEventListener('click',function(){
   // let word=document.getElementById('searchit').value;
   // localStorage.setItem('word',word);
   // window.location.reload();
+  clicked=true;
   if(div.innerHTML){
     div.innerHTML='';
-    console.log(div.innerHTML+" LOOL");
   }
-
+  console.log(option+' sur quoi on clique');
   DisplayMovies();
 });
 
@@ -85,3 +141,4 @@ document.getElementById('btnit').addEventListener('click',function(){
 document.getElementById('nav_l1').addEventListener('click',function(){
   console.log("LOOOOL");
 })
+
--- navbar.js
+++ navbar.js
@@ -5,7 +5,9 @@ navObj={
   'n4':document.getElementById('nav_l4'),
 };
 
+
 let divText=document.createElement('div');
+let option='';
 divText.setAttribute("id", "messageBox");
 
 let image = document.createElement('img');
@@ -14,43 +16,56 @@ image.setAttribute('id','famimg');
 let h1=document.createElement('h1');
 let h3=document.createElement('h3');
 
+let d=document.createElement('h2');
+
 window.onload=navL1();
 
 function navL1(){
+  option='home';
   navObj['n1'].style.pointerEvents = 'none';
   for(let i in navObj){
     if(i!='n1'){
-      console.log(i+" et "+navObj[i]);
       navObj[i].style.pointerEvents = 'auto';
     }
   }
-  
+
   divText.innerHTML='';
   image.src='fam.png';
-  
+  console.log(lol);
   h1.innerHTML='Unleash the Power of CineFlix';
   h3.innerHTML="Check out our Offerings!";
-
 
   divText.appendChild(h1);
   divText.appendChild(h3);
 
   document.body.appendChild(divText);
-  document.body.appendChild(image);
-  
+  document.body.appendChild(image);  
+  if(lol=='nice'){
+    // window.scrollTo(0, 547.3333129882812);
+    // if(window.scrollY!=547.3333129882812){
+    //   console.log('niceeeeeee');
+    //   div.scrollIntoView();  
+    // }
+    console.log(window.scrollY);
+    console.log(div);
+    d.innerHTML="FEATURED";
+    document.body.appendChild(d);
+    DisplayMovies();
+  }
 };
 
 
 navObj['n2'].addEventListener('click',function(){
- 
+  option='movie';
   navObj['n2'].style.pointerEvents = 'none';
   for(let i in navObj){
     if(i!='n2'){
-      console.log(i+" et "+navObj[i]);
+      // console.log(i+" et "+navObj[i]);
       navObj[i].style.pointerEvents = 'auto';
     }
   }  
   
+
   divText.innerHTML='';
 
   image.src = "movie.png";
@@ -58,19 +73,35 @@ navObj['n2'].addEventListener('click',function(){
   h1.innerHTML='Your Movie Adventure Starts Here';
   h3.innerHTML="Up Up and Away!";
 
-
   divText.appendChild(h1);
   divText.appendChild(h3);
 
   document.body.appendChild(divText);
   document.body.appendChild(image);
+  console.log(lol);
+  if(lol=='nice'){
+    // window.scrollTo(0, 547.3333129882812);
+    // if(window.scrollY!=547.3333129882812){
+    //   console.log('niceeeeeee');
+    //   div.scrollIntoView();  
+    // }
+    console.log(window.scrollY)
+    // let d=document.createElement('h2');
+    console.log(div.offsetTop);
+    console.log(div);
+    d.innerHTML="MOVIES";
+    document.body.appendChild(d);
+    DisplayMovies();
+
+  }
 });
 
 navObj['n3'].addEventListener('click',function(){
+  option='series';
   navObj['n3'].style.pointerEvents = 'none';
   for(let i in navObj){
     if(i!='n3'){
-      console.log(i+" et "+navObj[i]);
+      // console.log(i+" et "+navObj[i]);
       navObj[i].style.pointerEvents = 'auto';
     }
   }
@@ -78,26 +109,40 @@ navObj['n3'].addEventListener('click',function(){
 
   image.src = "series.png";
 
-  
   h1.innerHTML='Binge-Worthy TV Awaits';
   h3.innerHTML='Tune into Greatness...';
-
 
   divText.appendChild(h1);
   divText.appendChild(h3);
 
   document.body.appendChild(divText);
   document.body.appendChild(image);
+  if(lol=='nice'){
+    // window.scrollTo(0, 547.3333129882812);
+    // if(window.scrollY!=547.3333129882812){
+    //   console.log('niceeeeeee');
+    //   div.scrollIntoView();  
+    // }
+    console.log(window.scrollY)
+    console.log(div.offsetTop);
+    // let d=document.createElement('h2');
+    console.log(div);
+    d.innerHTML="TV";
+    document.body.appendChild(d);
+    DisplayMovies();
+  }
 });
 
 navObj['n4'].addEventListener('click',function(){
+  option='game';
   navObj['n4'].style.pointerEvents = 'none';
   for(let i in navObj){
     if(i!='n4'){
-      console.log(i+" et "+navObj[i]);
+      // console.log(i+" et "+navObj[i]);
       navObj[i].style.pointerEvents = 'auto';
     }
   }
+
   divText.innerHTML='';
 
   image.src = "games.png";
@@ -111,4 +156,22 @@ navObj['n4'].addEventListener('click',function(){
 
   document.body.appendChild(divText);
   document.body.appendChild(image);
+  if(lol=='nice'){
+    // window.scrollTo(0, 547.3333129882812);
+    // if(window.scrollY!=547.3333129882812){
+    //   console.log('niceeeeeee');
+    //   div.scrollIntoView();  
+    // }
+    console.log(window.scrollY);
+    console.log(div.offsetTop);
+    console.log(div);
+    // let d=document.createElement('h2');
+    d.innerHTML="GAMING";
+    document.body.appendChild(d);
+    DisplayMovies();
+  }
 });
+
+document.getElementById('searchit').addEventListener('click',function(){
+  window.scrollTo(0, 0);
+})
